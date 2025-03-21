7447def76ad1ab932ea09666e8d7d549b6e56109
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 09:57:36 +0530

    Automated commit: backend/routes/userRoutes.js

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,1 @@
+const express = require('express');const { getUser, createUser } = require('../controllers/userController');const router = express.Router();router.get('/', getUser);router.post('/', createUser);module.exports = router;
