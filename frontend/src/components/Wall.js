1b92c658a988bbec3535d44bdd7012e7a1c85e03
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 09:58:24 +0530

    Automated commit: frontend/src/components/Wall.js

---
+++ frontend/src/components/Wall.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Wall() {
+  return (
+    <div className="wall">
+      <svg>
+        <!-- SVG for wall -->
+      </svg>
+    </div>
+  );
+}
+
+export default Wall;
