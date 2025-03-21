131e9392937a6fb1c0dbf76e5b62dc6ce29b6211
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 09:58:18 +0530

    Automated commit: frontend/src/components/RunningMan.js

---
+++ frontend/src/components/RunningMan.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RunningMan() {
+  return (
+    <div className="running-man">
+      <svg>
+        <!-- SVG for running man -->
+      </svg>
+    </div>
+  );
+}
+
+export default RunningMan;
