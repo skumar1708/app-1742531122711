5bd8397c5fe4f276f4ca27cfc9e641c6965077ae
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 09:57:24 +0530

    Automated commit: backend/models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose');const userSchema = new mongoose.Schema({  username: {    type: String,    required: true  },  score: {    type: Number,    default: 0  }});module.exports = mongoose.model('User', userSchema);
